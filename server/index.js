4ea91c9e38b67481905e95dd62ba2514eae88bdf
saikiran0701 <reddysai616@gmail.com>
2023-11-13 22:11:06 +0530

    sampledb

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 require('dotenv').config();
-
-const port = process.env.PORT || 5000 ;
-
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/Schema');
+const connectDB = require('./config/db');
+const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.listen(port , console.log(`running on port , ${port}`))
+
+connectDB();
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql:true,
+  })
+);
+
+app.listen(port, console.log(`Server running on port ${port}`));
